f950a66b8826c652d4bb030bf9c087fc0eff4ea4
pedrojaokkj <jpdr3551@gmail.com>
2026-08-23 01:13:15 -0300

    first commit

---
+++ impressao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int n, i = 0;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++){
+        printf("%d", i);
+        printf("\n");
+    }
+    
+    
+    printf("hello world");
+    return 0;
+}
